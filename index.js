38abcea181c7e243b871712d7cc77529ff2f330b
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-04-04 00:27:28 +0300

    Done HW

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+let cat, dog, bird, bird2;
+class Cat{
+    constructor(name,gender){
+        this.name = name
+        this.gender =gender
+
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+class Dog{
+constructor(name,gender){
+    this.name = name
+    this.gender = gender
+}
+speak(){
+    return `${this.name} says woof!`
+}
+}
+class Bird{
+    constructor(name,gender){
+        this.name =name
+        this.gender = gender
+    }
+    speak(){
+        if(this.gender === "male")
+         return `It\'s me! ${this.name}, the parrot!`
+        else if (this.gender === "female") {
+              return `${this.name} says squawk!`
+        } 
+    }
+}
+
+cat = new Cat("Sasha", "female")
+dog = new Dog("Rufio", "male")
+bird = new Bird("Pablo", "male")
+bird2 = new Bird("Mable", "female")
